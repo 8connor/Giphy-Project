baad7967088a0c910ac47441d68ab66233f117aa
The-Napster <connorh16@gmail.com>
2020-06-24 03:06:10 -0400

    major changes to the functionality. The search now works properly.

--- assets/Java/app.js
+++ assets/Java/app.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
-    $("form").on("submit", function(){
+    $("form").on("submit", function () {
         return false;
     });
 
     $("#clearButton").hide();
-    $(".categoryButton").on("click", function () {
+
+    $(".categoryButton").on("click", function search() {
         $("#gifButtonDiv").empty();
         $("#clearButton").show();
+        
         var topics = $(this).attr("data-topics");
-
-        var giphy = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=TtNtOrejgWgfo7u3qG8DWCOjxAafwZqn";
+        var giphy = "https://api.giphy.com/v1/gifs/search?q="
+            + topics
+            + "&api_key=TtNtOrejgWgfo7u3qG8DWCOjxAafwZqn";
 
         $.ajax(
             {
@@ -17,20 +20,15 @@ $(document).ready(function () {
                 method: "GET",
             })
             .then(function (response) {
-
                 var results = response.data;
 
                 console.log(response);
 
                 for (var i = 0; i < results.length; i++) {
                     var gifButtonDiv = $("#gifButtonDiv");
-
                     var topicImage = $("<img>");
-
                     var rating = results[i].rating;
-
                     var p = $("<p>").text("Rating: " + rating);
-
                     var div = $("<div>");
 
                     topicImage.attr("src", results[i].images.fixed_height.url);
@@ -39,22 +37,60 @@ $(document).ready(function () {
                     div.append(p)
                     div.append(topicImage);
                 };
-            });
-        });
+            }
+        );
+    });
 
-        $("#clearButton").on("click", function(){
+    $("#clearButton").on("click", function () {
+        $("#gifButtonDiv").empty();
+        $(this).hide();
+    });
+
+    $("#addButton").on("click", function () {
+        var search = $("#userSearch").val()
+        var makeButton = $("<button>");
+
+        makeButton.addClass("categoryButton")
+        makeButton.attr("data-topics", search)
+        makeButton.attr("type", "button")
+        makeButton.html(search)
+
+        $(".buttons").append(makeButton)
+
+        $(makeButton).on("click", function search() {
             $("#gifButtonDiv").empty();
-            $(this).hide();
-        });
+            $("#clearButton").show();
 
-        $("#submitButton").on("click", function(){
-            var searchTerm = $("#addButton").val().trim();
+            var topics = $(this).attr("data-topics");
+            var giphy = "https://api.giphy.com/v1/gifs/search?q="
+                + topics
+                + "&api_key=TtNtOrejgWgfo7u3qG8DWCOjxAafwZqn";
 
-            
+            $.ajax(
+                {
+                    url: giphy,
+                    method: "GET",
+                })
+                .then(function (response) {
+                    var results = response.data;
 
+                    console.log(response);
 
-        });
+                    for (var i = 0; i < results.length; i++) {
+                        var gifButtonDiv = $("#gifButtonDiv");
+                        var topicImage = $("<img>");
+                        var rating = results[i].rating;
+                        var p = $("<p>").text("Rating: " + rating);
+                        var div = $("<div>");
 
-        
-    
+                        topicImage.attr("src", results[i].images.fixed_height.url);
+
+                        gifButtonDiv.append(div);
+                        div.append(p)
+                        div.append(topicImage);
+                    };
+                }
+            );
+        })
+    });
 });
